a415e73ec1baf04112927808278a7c85137c47a9
Justin Lee <justindavidlee88@gmail.com>
2018-01-30 18:56:19 -0800

    Add singly linked list

---
+++ python/linkedlist.py
@@ -0,0 +1,43 @@
+class Node:
+    _next = None
+    value = None
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return '%s' % str(self.value)
+
+    def __repr__(self):
+        return '<Node:%s>' % str(self)
+
+
+class LinkedList:
+    head = None
+
+    def __len__(self):
+        return sum(1 for i in self)
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node._next
+
+    def __str__(self):
+        return ', '.join(str(n) for n in self)
+
+    def add_first(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            node._next, self.head = self.head, node
+
+    def add_last(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            for n in self:
+                pass
+            else:
+                n._next = node
