44c59a1ceeeeb31ff15f0743bee61ab659d1363c
Justin Lee <justindavidlee88@gmail.com>
2018-02-04 18:56:06 -0800

    Refactor

---
+++ python/linkedlist/__init__.py
@@ -0,0 +1,41 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return '<SinglyNode:%s>' % str(self)
+
+
+class LinkedList:
+
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+
+    def __len__(self):
+        return sum(1 for i in self)
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    def __str__(self):
+        return ', '.join(str(n) for n in self)
+
+    def __getitem__(self, value):
+        for n in self:
+            if n.value == value:
+                return n
+        raise LookupError("Node cannot be found")
+
+    def __delitem__(self, value):
+        for n in self:
+            if n.next and n.next.value == value:
+                n.next = n.next.next
+                n.next.next = None
+                break
+        else:
+            raise KeyError("Node cannot be found")
